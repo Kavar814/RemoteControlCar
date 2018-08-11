78530f698172a17fb19f87c9405e57db7480cd09
Kavar814 <kavin.gmk@gmail.com>
2018-08-10 22:39:09 -0400

    Initial Commit

---
+++ RCCar.ino
@@ -0,0 +1,72 @@
+// connect motor controller pins to Arduino digital pins
+// motor one
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+// motor two
+int enB = 5;
+int in3 = 7;
+int in4 = 6;
+void setup()
+{
+  Serial.begin(9600);
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+void loop()
+{
+  if(Serial.available() > 0){
+    char inputChar = Serial.read();
+    switch(inputChar){
+      case 'w':
+          //running small motor
+          digitalWrite(in1, HIGH);
+          digitalWrite(in2, LOW);
+          analogWrite(enA, 255);//range 0-255  /*helps control speed*/
+          //running large motor
+          digitalWrite(in3, HIGH);
+          digitalWrite(in4, LOW);
+          analogWrite(enB, 255);
+        break;
+      case 'a':
+            digitalWrite(in1, HIGH);
+            digitalWrite(in2, LOW);
+            analogWrite(enA, 255);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+            analogWrite(enB, 255);
+        break;
+      case 's':
+            //running small motor
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, HIGH);
+            analogWrite(enA, 255);//range 0-255  /*helps control speed*/
+            //running large motor
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+            analogWrite(enB, 255);
+        break;
+      case 'd':
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, HIGH);
+            analogWrite(enA, 255);
+            digitalWrite(in3, HIGH);
+            digitalWrite(in4, LOW);
+            analogWrite(enB, 255);
+        break;
+      case ' ':
+             digitalWrite(in1, LOW);
+             digitalWrite(in2, LOW);
+             digitalWrite(in3, LOW);
+             digitalWrite(in4, LOW);
+        break; 
+    } 
+  }
+}
+
---
+++ RCCarBluetoothController/RCCarBluetoothController/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCCarBluetoothController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BT RC Controller v.1.0 by Kavin Govindarajan");
+            string input;
+            SerialPort serialPort = new SerialPort();
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM8";
+            serialPort.Open();
+            while (serialPort.IsOpen)
+            {
+                input = Convert.ToString(Console.ReadLine());
+                serialPort.Write(input);
+            }
+            Console.ReadKey();
+        }
+    }
+}
